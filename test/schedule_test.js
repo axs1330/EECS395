d780f708c5f4e673e5ef658b77b1afb97034289c
Amrish Selvam <axs1330@case.edu>
2020-04-04 17:16:23 -0400

    Create schedule_test.js

---
+++ test/schedule_test.js
@@ -0,0 +1,38 @@
+var test = require('unit.js');
+describe('schedule', function(){
+  it('parseScheduleFiles', function(){
+    // parse schedule given a schedule file list
+  });
+  it('naiveSchedule', function(){
+    /* psudocode:
+    load example schedule
+    
+    */
+    // naive schedule function tests
+    // this is for the main algorithm
+  });
+  it('getStartTime', function(){
+    // for the getStartTime function
+  });
+  it('getEndTime', function(){
+    // for the getEndTime function
+  });
+  it('getEventDate', function(){
+    // for the getEventDate function
+  });
+  it('getTime', function(){
+    // for the getTime function
+  });
+  it('getDate', function(){
+    // for the getDate function
+  });
+  it('time_diff', function(){
+    // for the time_diff function
+  });
+  it('earlier', function(){
+    // for the earlier function
+  });
+  it('time_add', function(){
+    // for the time_add function
+  });
+});
